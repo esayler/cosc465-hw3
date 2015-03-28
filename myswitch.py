57adf2a9e37c1408173d7ce41493b2d80c88aa9d
Eric Sayler <esayler@colgate.edu>
2015-03-28 15:59:12 -0400

    complete hw03

--- myswitch.py
+++ myswitch.py
@@ -12,23 +12,35 @@ from switchyard.lib.packet import *
 from switchyard.lib.common import *
 
 def switchy_main(net):
-    my_interfaces = net.interfaces() 
-    mymacs = [intf.ethaddr for intf in my_interfaces]
+    my_interfaces = net.interfaces()
+    my_addresses = [interface.ethaddr for interface in my_interfaces]
+    my_forwarding_table = {}
 
     while True:
         try:
-            dev,packet = net.recv_packet()
+            port_in, packet = net.recv_packet()
         except NoPackets:
             continue
         except Shutdown:
             return
 
-        log_debug ("In {} received packet {} on {}".format(net.name, packet, dev))
-        if packet[0].dst in mymacs:
+        log_debug ("In {} received packet {} on {}".format(net.name, packet, port_in))
+
+        source_address = packet[0].src
+        destination_address = packet[0].dst
+
+        my_forwarding_table[source_address] = port_in
+
+        if destination_address in my_addresses:
             log_debug ("Packet intended for me")
+            #drop for now
         else:
-            for intf in my_interfaces:
-                if dev != intf.name:
-                    log_debug ("Flooding packet {} to {}".format(packet, intf.name))
-                    net.send_packet(intf.name, packet)
+            if packet[0].dst in my_forwarding_table:
+                net.send_packet(my_forwarding_table[destination_address], packet)
+            else:
+                for interface in my_interfaces:
+                    if port_in != interface.name:
+                        log_debug ("Flooding packet {} to {}".format(packet, interface.name))
+                        net.send_packet(interface.name, packet)
+
     net.shutdown()
